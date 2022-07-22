42f40ab25837fb04b28e4d7e23ca5fc04ef04900
Manoj Gawade <gawade.manoj7@gmail.com>
2022-07-22 17:43:28 +0530

    Twitter link added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,9 +22,9 @@ const Footer = () => {
 
     <div className="footer__socials">
 
-      <a href="https://www.facebook.com/manoj.gawade"><BsFacebook /></a>
-      <a href="https://www.instagram.com/manojgawade24"><BsInstagram /></a>
-      <a href="https://twitter.com"><BsTwitter /></a>
+      <a href="https://www.facebook.com/manoj.gawade" rel="noreferrer" target='_blank'><BsFacebook /></a>
+      <a href="https://www.instagram.com/manojgawade24" rel="noreferrer" target='_blank'><BsInstagram /></a>
+      <a href="https://twitter.com/ManojGawade24" rel="noreferrer" target='_blank'><BsTwitter /></a>
     </div>
     <div className="footer__copyright">
       <small>
